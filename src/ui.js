6d154532ba8a689bb48e2517e1aaee273058ac1a
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-04 20:58:38 +0200

    fixed multiple cancel buttons bug

--- src/ui.js
+++ src/ui.js
@@ -81,17 +81,19 @@ class UI {
       this.postSubmit.textContent = 'Update Post';
       this.postSubmit.className = 'post-submit btn btn-warning btn-block';
 
-      // Create cancel button
-      const button = document.createElement('button');
-      button.className = 'post-cancel btn btn-light btn-block';
-      button.appendChild(document.createTextNode('Cancel Edit'));
+      if (!document.querySelector('.post-cancel')) {
+        // Create cancel button
+        const button = document.createElement('button');
+        button.className = 'post-cancel btn btn-light btn-block';
+        button.appendChild(document.createTextNode('Cancel Edit'));
 
-      // Get parent
-      const cardForm = document.querySelector('.card-form');
-      // Get element to insert before
-      const formEnd = document.querySelector('.form-end');
-      // Insert cancel button
-      cardForm.insertBefore(button, formEnd);
+        // Get parent
+        const cardForm = document.querySelector('.card-form');
+        // Get element to insert before
+        const formEnd = document.querySelector('.form-end');
+        // Insert cancel button
+        cardForm.insertBefore(button, formEnd);
+      }
     } else {
       this.postSubmit.textContent = 'Post It';
       this.postSubmit.className = 'post-submit btn btn-primary btn-block';
