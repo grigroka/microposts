102831b1e4ec0fe98e4e59d456570d53233f238e
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-04 20:55:49 +0200

    update post

--- src/app.js
+++ src/app.js
@@ -29,24 +29,39 @@ function getPosts() {
 function submitPost() {
   const title = document.querySelector('#title').value;
   const body = document.querySelector('#body').value;
+  const id = document.querySelector('#id').value;
 
+  const data = {
+    title,
+    body
+  };
+
+  // Validate input
   if (title === '' || body === '') {
     ui.showAlert('Please fill in all fields', 'alert alert-danger');
   } else {
-    const data = {
-      title,
-      body
-    };
-
-    // Create Post
-    http
-      .post('http://localhost:3000/posts', data)
-      .then(() => {
-        ui.showAlert('Post added', 'alert alert-success');
-        ui.clearFields();
-        getPosts();
-      })
-      .catch(err => console.log(err));
+    // Check for ID
+    if (id === '') {
+      // Create Post
+      http
+        .post('http://localhost:3000/posts', data)
+        .then(() => {
+          ui.showAlert('Post added', 'alert alert-success');
+          ui.clearFields();
+          getPosts();
+        })
+        .catch(err => console.log(err));
+    } else {
+      // Update Post
+      http
+        .put(`http://localhost:3000/posts/${id}`, data)
+        .then(() => {
+          ui.showAlert('Post updated', 'alert alert-success');
+          ui.changeFormState('add');
+          getPosts();
+        })
+        .catch(err => console.log(err));
+    }
   }
 }
 
