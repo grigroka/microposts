6e04d937d3c7b59589220607a61f23ad2d48f3ed
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-04 20:31:44 +0200

    edit state

--- src/app.js
+++ src/app.js
@@ -11,6 +11,9 @@ document.querySelector('.post-submit').addEventListener('click', submitPost);
 // Listen for delete
 document.querySelector('#posts').addEventListener('click', deletePost);
 
+// Listen for edit state
+document.querySelector('#posts').addEventListener('click', enableEdit);
+
 // Get Posts
 function getPosts() {
   http
@@ -42,7 +45,6 @@ function submitPost() {
 
 // Delete Post
 function deletePost(e) {
-  e.preventDefault();
   if (e.target.parentElement.classList.contains('delete')) {
     const id = e.target.parentElement.dataset.id;
     if (confirm('Are you sure?')) {
@@ -55,4 +57,25 @@ function deletePost(e) {
         .catch(err => console.log(err));
     }
   }
+  e.preventDefault();
+}
+
+// Enable Edit State
+function enableEdit(e) {
+  if (e.target.parentElement.classList.contains('edit')) {
+    const id = e.target.parentElement.dataset.id;
+    const title = e.target.parentElement.previousElementSibling.textContent;
+    const body = e.target.parentElement.previousElementSibling.textContent;
+
+    const data = {
+      id,
+      title,
+      body
+    };
+
+    // Fill form with curent post
+    ui.fillForm(data);
+  }
+
+  e.preventDefault();
 }
